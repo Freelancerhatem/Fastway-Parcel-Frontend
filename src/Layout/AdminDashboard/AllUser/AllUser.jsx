c7b4337772ff0865871dcc3320e15861021931eb
Freelancerhatem <108505344+Freelancerhatem@users.noreply.github.com>
2023-11-26 15:11:01 +0600

    deliverman modified

--- src/Layout/AdminDashboard/AllUser/AllUser.jsx
+++ src/Layout/AdminDashboard/AllUser/AllUser.jsx
@@ -3,28 +3,52 @@ import UsersCard from "./UsersCard";
 import useUsers from "../../../Hooks/useUsers";
 import toast, { Toaster } from "react-hot-toast";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure/useAxiosSecure";
+import Swal from "sweetalert2";
 
 
 const AllUser = () => {
-    const [users,refetch] = useUsers();
+    const [users, refetch] = useUsers();
     const axiosSecure = useAxiosSecure()
-    
-    const handleDeliveryMan=(id)=>{
-        const userData = users.find(user=>user._id===id);
-        const{name,userType} = userData
-        
-        axiosSecure.patch(`/users/admin/${id}`)
-        .then(res=>{
-            if(res.data.modifiedCount>0){
-                refetch();
-                toast.success(`${name} ${userType}`);
-                
+
+    const handleDeliveryMan = (id) => {
+        const userData = users.find(user => user._id === id);
+        const { name, userType } = userData
+
+        Swal.fire({
+            title: "Do You Want?",
+            text: `${name} Make A Delivery  Man`,
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#1db7ad",
+            cancelButtonColor: "#fac900",
+            confirmButtonText: "Yes"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                axiosSecure.patch(`/users/admin/${id}`)
+                    .then(res => {
+                        if (res.data.modifiedCount > 0) {
+                            refetch();
+                            // toast.success(`${name} ${userType}`);
+
+                        }
+                    })
+
+
+
+                Swal.fire({
+                    title: "Success!",
+                    text: `${name} is now a Delivery Man`,
+                    icon: "success"
+                });
             }
-        })
+        });
 
 
 
-        toast.success(id)
+
+
+
     }
     return (
         <div className=" px-14">
@@ -40,7 +64,7 @@ const AllUser = () => {
                         <td >Total Amount</td>
                         <td >Total Amount</td>
                         <td >Total Amount</td>
-                        
+
 
 
                     </tr>
@@ -48,7 +72,7 @@ const AllUser = () => {
                 <tbody className="">
 
                     {
-                        users?.map((userData, index) =><UsersCard key={index} handleDeliveryMan={handleDeliveryMan} index={index} userData={userData}></UsersCard>) 
+                        users?.map((userData, index) => <UsersCard key={index} handleDeliveryMan={handleDeliveryMan} index={index} userData={userData}></UsersCard>)
                     }
                 </tbody>
 
