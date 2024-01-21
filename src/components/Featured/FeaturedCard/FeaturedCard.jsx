569c804e3edd939bf25006f29e324b5c16715a4d
Freelancerhatem <108505344+Freelancerhatem@users.noreply.github.com>
2024-01-21 12:39:30 +0600

    number count start on scroll trigger fixed

--- src/components/Featured/FeaturedCard/FeaturedCard.jsx
+++ src/components/Featured/FeaturedCard/FeaturedCard.jsx
@@ -9,7 +9,7 @@ import support from '../../../assets/animation/support.json'
 const FeaturedCard = ({singledata}) => {
     const {id,title,description} = singledata;
     return (
-        <div className=' bg-gray-50 hover:bg-green-100 shadow rounded-lg hover:transition-all p-5 hover:translate-y-1'>
+        <div className=' bg-gray-50  shadow rounded-lg hover:transition-all p-5 hover:translate-y-1'>
             <LottieAnimation anim={id=='1'?fast: id=='2'? safety:support}></LottieAnimation>
             <h1 className='text-center bg-yellow-200  rounded-xl text-2xl font-semibold'>{title}</h1>
             <p className=' text-justify mt-5'>{description}</p>
--- src/components/HomeStatics/HomeStatics.jsx
+++ src/components/HomeStatics/HomeStatics.jsx
@@ -2,58 +2,61 @@ import { useEffect, useState } from "react";
 import useAllParcel from "../../Hooks/useAllParcel/useAllParcel";
 import useUsers from "../../Hooks/useUsers";
 import NumberCount from "../NumberCount/NumberCount";
+import ScrollTrigger from "react-scroll-trigger";
 
 
 
 const HomeStatics = () => {
-    const[allparcelData]=useAllParcel();
-    
+    const [allparcelData] = useAllParcel();
 
-    const [users,refetch] = useUsers();
+
+    const [users, refetch] = useUsers();
     const TotalUser = users?.length;
 
-    const[BookedPercel,setBookedPercel] = useState([]);
-    const[deliveredData,setdeliveredData] = useState([]);
-
-    useEffect(()=>{
-        const filterDatas=allparcelData.filter(filterData=>filterData.status==='On The Way')
+    const [BookedPercel, setBookedPercel] = useState([]);
+    const [deliveredData, setdeliveredData] = useState([]);
+    const [counterOn, setCounterOn] = useState(false)
+    useEffect(() => {
+        const filterDatas = allparcelData.filter(filterData => filterData.status === 'On The Way')
         setBookedPercel(filterDatas);
-        
 
-    },[allparcelData]);
-    useEffect(()=>{
-        const filterDeliverdDatas=allparcelData.filter(filterDeliveryData=>filterDeliveryData.status==='delivered')
+
+    }, [allparcelData]);
+    useEffect(() => {
+        const filterDeliverdDatas = allparcelData.filter(filterDeliveryData => filterDeliveryData.status === 'delivered')
         setdeliveredData(filterDeliverdDatas);
-    },[allparcelData])
-    
+    }, [allparcelData])
+
     return (
-        <div className="stats shadow max-w-[100vw] ">
-            
-            <div className="flex flex-col lg:flex-row">
-            <div className="stat">
-                
-                <div className="stat-title text-xl font-bold text-black">Total Users</div>
-                <div className="stat-value text-primary text-center"><NumberCount endValue={TotalUser}></NumberCount></div>
-                
-            </div>
+        <ScrollTrigger onEnter={() => setCounterOn(true)} onExit={() => setCounterOn(false)}>
+            <div className="stats shadow max-w-[100vw] ">
 
-            <div className="stat">
-                
-                <div className="stat-title text-xl font-bold text-black"> Parcel Booked</div>
-                <div className="stat-value text-secondary text-center"><NumberCount endValue={BookedPercel.length}></NumberCount></div>
-                
-            </div>
+                <div className="flex flex-col lg:flex-row">
+                    <div className="stat">
 
-            <div className="stat">
-            <div className="stat-title text-xl font-bold text-black"> Parcel Delivered</div>
-                <div className="stat-value text-center"><NumberCount endValue={deliveredData.length}></NumberCount></div>
-                
-                
-            </div>
-            
-            </div>
+                        <div className="stat-title text-xl font-bold text-black">Total Users</div>
+                        <div className="stat-value text-primary text-center">{counterOn && <NumberCount endValue={TotalUser}></NumberCount>}</div>
 
-        </div>
+                    </div>
+
+                    <div className="stat">
+
+                        <div className="stat-title text-xl font-bold text-black"> Parcel Booked</div>
+                        <div className="stat-value text-secondary text-center">{counterOn && <NumberCount endValue={BookedPercel.length}></NumberCount>}</div>
+
+                    </div>
+
+                    <div className="stat">
+                        <div className="stat-title text-xl font-bold text-black"> Parcel Delivered</div>
+                        <div className="stat-value text-center">{counterOn && <NumberCount endValue={deliveredData.length}></NumberCount>}</div>
+
+
+                    </div>
+
+                </div>
+
+            </div>
+        </ScrollTrigger>
     );
 };
 
